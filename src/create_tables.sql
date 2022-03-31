98c11f18c111e09c28371d70eb49912b51417ff1
tedro <sktedro@gmail.com>
2022-03-31 22:56:43 +0200

    Added some initial values

--- src/create_tables.sql
+++ src/create_tables.sql
@@ -21,11 +21,11 @@ DROP TABLE "smuggler" CASCADE CONSTRAINTS;
 DROP TABLE "oversees";
 DROP TABLE "shift";
 DROP TABLE "warden";
-DROP TABLE "jail" CASCADE CONSTRAINTS;
+DROP TABLE "prison" CASCADE CONSTRAINTS;
 
 -- Create new tables
 
-CREATE TABLE "jail" (
+CREATE TABLE "prison" (
     "id" INT GENERATED ALWAYS AS IDENTITY NOT NULL PRIMARY KEY,
     "region" VARCHAR(64),
     "city" VARCHAR(64) NOT NULL,
@@ -38,9 +38,9 @@ CREATE TABLE "warden" (
     "id" INT GENERATED ALWAYS AS IDENTITY NOT NULL PRIMARY KEY,
     "name" VARCHAR(64) NOT NULL,
     "surname" VARCHAR(64) NOT NULL,
-    "jail_id" INT NOT NULL,
-    CONSTRAINT "warden_jail_id_pk"
-            FOREIGN KEY ("jail_id") REFERENCES "jail" ("id")
+    "prison_id" INT NOT NULL,
+    CONSTRAINT "warden_prison_id_pk"
+            FOREIGN KEY ("prison_id") REFERENCES "prison" ("id")
             ON DELETE CASCADE
 );
 
@@ -58,7 +58,7 @@ CREATE TABLE "oversees" (
     PRIMARY KEY ("warden_id", "shift_id"),
     CONSTRAINT "oversees_warden_id_pk"
             FOREIGN KEY ("warden_id") REFERENCES "warden" ("id")
-            ON DELETE SET NULL,
+            ON DELETE CASCADE,
     CONSTRAINT "oversees_shift_id_pk"
             FOREIGN KEY ("shift_id") REFERENCES "shift" ("id")
             ON DELETE CASCADE
@@ -72,7 +72,7 @@ CREATE TABLE "smuggler" (
     "iban" VARCHAR2(34), -- We don't need his IBAN if he doesn't want to get paid
     "birth_number" NUMBER(10, 0) NOT NULL,
     CONSTRAINT "phone_number_check"
-            CHECK (REGEXP_LIKE("phone_number", '^+\d{12}$')),
+            CHECK (REGEXP_LIKE("phone_number", '^(\+\d{12})$')),
     CONSTRAINT "iban_length_check"
             CHECK (REGEXP_LIKE("iban", '^[A-Z]{2}\d+$') AND LENGTH("iban") <= 34)
 );
@@ -91,13 +91,13 @@ CREATE TABLE "agreement" (
 
 CREATE TABLE "partnership" (
   "smuggler_id" INT NOT NULL,
-  "jail_id" INT NOT NULL,
-  PRIMARY KEY ("smuggler_id", "jail_id"),
+  "prison_id" INT NOT NULL,
+  PRIMARY KEY ("smuggler_id", "prison_id"),
   CONSTRAINT "partnership_smuggler_id_pk"
           FOREIGN KEY ("smuggler_id") REFERENCES "smuggler" ("id")
           ON DELETE CASCADE,
-  CONSTRAINT "partnership_jail_id_pk"
-          FOREIGN KEY ("jail_id") REFERENCES "jail" ("id")
+  CONSTRAINT "partnership_prison_id_pk"
+          FOREIGN KEY ("prison_id") REFERENCES "prison" ("id")
           ON DELETE CASCADE
 );
 
@@ -105,11 +105,11 @@ CREATE TABLE "customer" (
     "id" INT GENERATED ALWAYS AS IDENTITY NOT NULL PRIMARY KEY,
     "name" VARCHAR(64) NOT NULL,
     "surname" VARCHAR(64) NOT NULL,
-    "jail_id" INT,
+    "prison_id" INT,
     "cell_number" INT,
     "cell_type" VARCHAR(16),
-    CONSTRAINT "customer_jail_id_pk"
-            FOREIGN KEY ("jail_id") REFERENCES "jail" ("id")
+    CONSTRAINT "customer_prison_id_pk"
+            FOREIGN KEY ("prison_id") REFERENCES "prison" ("id")
             ON DELETE SET NULL
             -- the customer might be transferred to a different one
 );
@@ -119,7 +119,7 @@ CREATE TABLE "order" (
     "order_datetime" TIMESTAMP(0) NOT NULL,
     "delivery_datetime" TIMESTAMP(0),
     "delivery_method" VARCHAR(64),
-    "smuggler_id" INT NOT NULL,
+    "smuggler_id" INT,
     "customer_id" INT NOT NULL,
     CONSTRAINT "check_order_datetime"
             CHECK ("order_datetime" < "delivery_datetime"),
@@ -128,7 +128,7 @@ CREATE TABLE "order" (
             ON DELETE SET NULL,
     CONSTRAINT "order_customer_id_pk"
             FOREIGN KEY ("customer_id") REFERENCES "customer" ("id")
-            ON DELETE SET NULL
+            ON DELETE CASCADE
 );
 
 CREATE TABLE "pastry" (
@@ -184,26 +184,123 @@ CREATE TABLE "item" (
     "width" INT NOT NULL,
     "length" INT NOT NULL,
     "height" INT NOT NULL,
-    "price" NUMBER(*,2)
+    "wholesale_price" NUMBER(*, 2)
 );
 
 CREATE TABLE "order_content" (
     "order_id" INT NOT NULL,
     "pastry_id" INT NOT NULL,
     "item_id" INT DEFAULT NULL,
+    "amount" INT NOT NULL,
     PRIMARY KEY ("order_id", "pastry_id", "item_id"),
     CONSTRAINT "order_content_order_id_fk"
 		FOREIGN KEY ("order_id") REFERENCES "order" ("id")
 		ON DELETE CASCADE,
 	CONSTRAINT "order_content_pastry_id_fk"
 		FOREIGN KEY ("pastry_id") REFERENCES "pastry" ("id")
-		ON DELETE SET NULL,
+		ON DELETE CASCADE,
 	CONSTRAINT "order_content_item_id_fk"
 		FOREIGN KEY ("item_id") REFERENCES "item" ("id")
-		ON DELETE SET NULL
+		ON DELETE CASCADE
 );
 
 -- Insert some data
 
--- INSERT INTO "smuggler" ("name", "surname", "phone_number", "iban", "birth_number")
---     VALUES ('patrik', 'skalos', '+421944306657', 'SK1235421', 1010567890);
+-- Insert some ingredients
+INSERT INTO "ingredient" ("name", "current_amount", "unit", "wholesale_price")
+        VALUES ('flour', '10', 'kg', '0.3');
+INSERT INTO "ingredient" ("name", "current_amount", "unit", "wholesale_price")
+        VALUES ('egg', '83', 'pcs', '0.05');
+INSERT INTO "ingredient" ("name", "current_amount", "unit", "wholesale_price")
+        VALUES ('yeast', '1283', 'g', '0.016');
+INSERT INTO "ingredient" ("name", "current_amount", "unit", "wholesale_price")
+        VALUES ('garlic', '300', 'g', '0.023');
+
+-- Insert allergens
+INSERT INTO "allergen" ("name", "description") VALUES ('gluten', '');
+INSERT INTO "allergen" ("name", "description") VALUES ('eggs', '');
+INSERT INTO "allergen" ("name", "description") VALUES ('lupin', '');
+INSERT INTO "allergen" ("name", "description") VALUES ('milk', '');
+INSERT INTO "allergen" ("name", "description") VALUES ('nuts', '');
+INSERT INTO "allergen" ("name", "description") VALUES ('peanuts', '');
+INSERT INTO "allergen" ("name", "description") VALUES ('soy', '');
+
+-- Connect ingredients and allergens
+INSERT INTO "ingredient_allergen" ("ingredient_id", "allergen_id")
+        VALUES(1, 1); -- flour:gluten
+INSERT INTO "ingredient_allergen" ("ingredient_id", "allergen_id")
+        VALUES(1, 3); -- flout:lupin
+INSERT INTO "ingredient_allergen" ("ingredient_id", "allergen_id")
+        VALUES(2, 2); -- egg:eggs
+INSERT INTO "ingredient_allergen" ("ingredient_id", "allergen_id")
+        VALUES(3, 5); -- yeast:nuts
+INSERT INTO "ingredient_allergen" ("ingredient_id", "allergen_id")
+        VALUES(3, 6); -- yeast:peanuts
+INSERT INTO "ingredient_allergen" ("ingredient_id", "allergen_id")
+        VALUES(3, 7); -- yeast:soy
+
+-- Insert some pastries
+INSERT INTO "pastry" ("name", "type", "weight")
+        VALUES ('classic bread', 'bread', '1000');
+INSERT INTO "pastry" ("name", "type", "weight")
+        VALUES ('garlic bread', 'bread', '500');
+
+-- Connect pastries to ingredients
+INSERT INTO "pastry_ingredients" ("pastry_id", "ingredient_id")
+        VALUES(1, 1); -- classic_bread:flour
+INSERT INTO "pastry_ingredients" ("pastry_id", "ingredient_id")
+        VALUES(1, 2); -- classic_bread:egg
+INSERT INTO "pastry_ingredients" ("pastry_id", "ingredient_id")
+        VALUES(1, 3); -- classic_bread:yeast
+INSERT INTO "pastry_ingredients" ("pastry_id", "ingredient_id")
+        VALUES(2, 1); -- garlic_bread:flour
+INSERT INTO "pastry_ingredients" ("pastry_id", "ingredient_id")
+        VALUES(2, 2); -- garlic_bread:egg
+INSERT INTO "pastry_ingredients" ("pastry_id", "ingredient_id")
+        VALUES(2, 3); -- garlic_bread:yeast
+INSERT INTO "pastry_ingredients" ("pastry_id", "ingredient_id")
+        VALUES(2, 4); -- garlic_bread:garlic
+
+-- Insert some items that can be baked into a pastry
+INSERT INTO "item" ("name", "description", "width", "length", "height", "wholesale_price")
+        VALUES('knife', 'kitchen knife', 18, 200, 24, 40);
+INSERT INTO "item" ("name", "description", "width", "length", "height", "wholesale_price")
+        VALUES('wrench', 'M8 stainless steel metric wrench', 8, 122, 40, 24);
+INSERT INTO "item" ("name", "description", "width", "length", "height", "wholesale_price")
+        VALUES('scalpel', 'carbonated steel with anti-slip handle', 10, 100, 10, 18);
+INSERT INTO "item" ("name", "description", "width", "length", "height", "wholesale_price")
+        VALUES('screwdriver', 'phillips screwdriver #4', 28, 155, 28, 42);
+
+-- Initialize two prisons
+INSERT INTO "prison" ("region", "city", "zip", "street", "street_number")
+        VALUES ('Trnavský kraj', 'Leopoldov', '92041', 'Väzničné námestie', '10');
+INSERT INTO "prison" ("city", "zip", "street", "street_number")
+        VALUES ('Košice', '04020', 'Lunícka', '9');
+
+-- Initialize some wardens
+INSERT INTO "warden" ("name", "surname", "prison_id")
+        VALUES ('John', 'Eyeless', 1);
+INSERT INTO "warden" ("name", "surname", "prison_id")
+        VALUES ('Bob', 'Guard', 1);
+INSERT INTO "warden" ("name", "surname", "prison_id")
+        VALUES ('Natalie', 'Harsh', 2);
+
+-- Initialize some smugglers
+INSERT INTO "smuggler" ("name", "surname", "phone_number", "birth_number")
+        VALUES ('Sam', 'Sneaky', '+421944333666', 0010166606);
+INSERT INTO "smuggler" ("name", "surname", "phone_number", "iban", "birth_number")
+        VALUES ('Robin', 'Hood', '+420111222333', 'CZ3601000000000123456789', 0001041444);
+
+-- Connect smugglers to prisons (partnership)
+
+-- Connect smugglers to wardens (agreement)
+
+-- Create some shifts
+
+-- Connect wardens to shifts
+
+-- Initialize some customers
+
+-- Create some orders
+
+-- Add contents of an order to an order
